f493da81762addb00d7e68269915ca32bdc625b2
Clark <daprogrammer1@gmail.com>
2022-09-17 22:01:21 +0800

    add static types at downloadCsv.ts

--- src/utils/downloadCsv.ts
+++ src/utils/downloadCsv.ts
@@ -1,12 +1,14 @@
-export const downloadCsv = (lapsArray: Array<any>): void => {
-  let csvString = "";
+import { Lap } from "./types";
+
+export const downloadCsv = (lapsArray: Array<Lap>): void => {
+  let csvString: string = "";
   for (let x of lapsArray) {
     const { time, number } = x;
     csvString += `${number}, ${time}\n`;
   }
-  const blob = new Blob([csvString], { type: "text/plain" });
-  const blobUrl = URL.createObjectURL(blob);
-  const anchor = document.createElement("a");
+  const blob: Blob = new Blob([csvString], { type: "text/plain" });
+  const blobUrl: string = URL.createObjectURL(blob);
+  const anchor: HTMLAnchorElement = document.createElement("a");
   anchor.href = blobUrl;
   anchor.download = "laps.csv";
   document.body.appendChild(anchor);
