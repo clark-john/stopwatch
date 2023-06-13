4eea9ba712a1d94147d88fba974b8889a521fe9f
clark-john <mjohncl4rk@gmail.com>
2023-06-14 01:01:05 +0800

    update downloadCsv.ts

--- src/utils/downloadCsv.ts
+++ src/utils/downloadCsv.ts
@@ -7,17 +7,20 @@
 import { Lap } from "./types";
 
 export const downloadCsv = (lapsArray: Array<Lap>): void => {
-	let csvString: string = "";
-	for (let x of lapsArray) {
-		const { time, number } = x;
-		csvString += `${number}, ${time}\n`;
+	if (!lapsArray.length) {
+		alert("Laps empty");
+	} else {
+		let csvString: string = "";
+		for (let x of lapsArray) {
+			const { time, number } = x;
+			csvString += `${number}, ${time}\n`;
+		}
+		const blob: Blob = new Blob([csvString], { type: "text/plain" });
+		const blobUrl: string = URL.createObjectURL(blob);
+		const anchor: HTMLAnchorElement = document.createElement("a");
+		anchor.href = blobUrl;
+		anchor.download = "laps.csv";
+		anchor.click();
+		anchor.remove();		
 	}
-	const blob: Blob = new Blob([csvString], { type: "text/plain" });
-	const blobUrl: string = URL.createObjectURL(blob);
-	const anchor: HTMLAnchorElement = document.createElement("a");
-	anchor.href = blobUrl;
-	anchor.download = "laps.csv";
-	document.body.appendChild(anchor);
-	anchor.click();
-	document.body.removeChild(anchor);
 };
